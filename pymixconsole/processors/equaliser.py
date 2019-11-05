71b6bd88f3f0b73b4973351c63ffb08be3aa1c87
csteinmetz1 <csteinmetz1@gmail.com>
2019-11-05 16:46:59 +0100

    improvements to EQ

--- pymixconsole/processors/equaliser.py
+++ pymixconsole/processors/equaliser.py
@@ -3,17 +3,17 @@ import numpy as np
 from ..processor import Processor
 from ..components.irrfilter import IIRfilter
 
-default_bands = [{"filter_type" : "low_shelf",  "gain" : 12.0, "Fc" : 80.0,    "Q" : 0.707},
-                 {"filter_type" : "peaking",    "gain" : 0.0, "Fc" : 200.0,   "Q" : 0.707},
-                 {"filter_type" : "peaking",    "gain" : -60.0, "Fc" : 1000.0,  "Q" : 5.0},
-                 {"filter_type" : "peaking",    "gain" : 0.0, "Fc" : 5000.0,  "Q" : 0.707},
+default_bands = [{"filter_type" : "low_shelf",  "gain" : 0.0, "Fc" :    80.0, "Q" : 0.707},
+                 {"filter_type" : "peaking",    "gain" : 0.0, "Fc" :   200.0, "Q" : 0.707},
+                 {"filter_type" : "peaking",    "gain" : 0.0, "Fc" :  1000.0, "Q" : 0.707},
+                 {"filter_type" : "peaking",    "gain" : 0.0, "Fc" :  5000.0, "Q" : 0.707},
                  {"filter_type" : "high_shelf", "gain" : 0.0, "Fc" : 10000.0, "Q" : 0.707}]
 
 class Equaliser(Processor):
     """ N band parametreic equalizer with customizable filter shapes
 
     """
-    def __init__(self, in_gain_val=1.0, out_gain_val=1.0, bands=default_bands, sample_rate=44100):
+    def __init__(self, in_gain_val=0.0, out_gain_val=0.0, bands=default_bands, sample_rate=44100):
         super().__init__("Equaliser", 
                         {"in_gain" : in_gain_val,
                          "out_gain": out_gain_val,
@@ -31,11 +31,17 @@ class Equaliser(Processor):
 
         return filters
 
-
     def process(self, data):
         
         for irrfilter in self.filters:
+            # apply input gain
+            data *= self.db2linear(self.parameters['in_gain'])
+
+            # apply filter
             data = irrfilter.apply_filter(data)
+
+            # apply output gain
+            data *= self.db2linear(self.parameters['out_gain'])
 
         return data
 
