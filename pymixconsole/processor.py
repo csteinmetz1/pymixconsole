d26e60c79bca9c376dd1f4c6dbc492caa60e0dfc
csteinmetz1 <csteinmetz1@gmail.com>
2019-11-06 16:10:08 +0100

    update to start standardization of process (we include methods to convert between dB and linear)

--- pymixconsole/processor.py
+++ pymixconsole/processor.py
@@ -1,5 +1,15 @@
+import numpy as np
+
 class Processor():
-    def __init__(self, name, parameters, sample_rate):
-        self.name = name
-        self.parameters = parameters
+    def __init__(self, name, parameters, block_size, sample_rate):
+        
+        self.name        = name
+        self.parameters  = parameters
+        self.block_size  = block_size
         self.sample_rate = sample_rate
+
+    def db2linear(self, value):
+        return np.power(10, value/20)
+
+    def linear2db(self, value):
+        return 20 * np.log10(value)
