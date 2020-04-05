1c58e7b4428081dfe6d41e033bab6be1d441a4d4
csteinmetz1 <csteinmetz1@gmail.com>
2020-04-05 12:36:34 +0200

    some clean to convolutional reverb before final

--- pymixconsole/processors/convreverb.py
+++ pymixconsole/processors/convreverb.py
@@ -33,7 +33,7 @@ class ConvolutionalReverb(Processor):
             self.parameters = ParameterList()
             self.parameters.add(Parameter("bypass",   False,   "bool", processor=None, p=0.1))
             self.parameters.add(Parameter("type", "sm-room", "string", processor=self, options=["sm-room", "md-room", "lg-room", "hall", "plate"]))
-            self.parameters.add(Parameter("decay",      1.0,  "float", processor=self, minimum=0.0, maximum=1.0))
+            self.parameters.add(Parameter("decay",      1.0,  "float", processor=self, minimum=0.1, maximum=1.0))
             self.parameters.add(Parameter("dry_mix",    1.0,  "float", processor=self, minimum=0.0, maximum=1.0))
             self.parameters.add(Parameter("wet_mix",    0.0,  "float", processor=self, minimum=0.0, maximum=1.0))
 
@@ -46,23 +46,20 @@ class ConvolutionalReverb(Processor):
         if x.ndim < 2: # if input is mono (samples,) add stereo dim
             x = np.expand_dims(x, 1)    
         
-        if x.shape[1] == 1: # if input is mono copy L to R        
+        if x.shape[1] == 1: # if input is mono copy L to R       
+            mono = True 
             x = np.repeat(x, 2, axis=1)
 
-        if self.parameters.bypass.value:
+        if self.parameters.bypass.value or self.parameters.wet_mix.value == 0.0:
             return x
         else:
-            #x = np.pad(x, ((0, self.block_size),(0,0))) # zero pad the input frame
-            #self.X = np.roll(self.X, 1, axis=2)         # make space for the new frame
-            #self.X[:,:,0] = fft(x, axis=0)              # store the result of the fft for current frame
-            #Y = np.sum(self.X * self.H, axis=2)         # multiply inputs with filters
-            #y = np.real(ifft(Y, axis=0))                # convert result to the time domain (only take real part)
+            # perform partitioned convolution
+            y = scipy.signal.fftconvolve(x, self.h, axes=0, mode='full')
 
-            y = scipy.signal.oaconvolve(x, self.h, axes=0, mode='full')
-            
+            # pick out the previous overlap that will be added to output
             overlap = self.overlap[:self.block_size]
 
-            # there will be some overlap left over we need to save
+            # there may be some overlap left over we need to save again
             eoverlap = self.overlap[self.block_size:]
             if eoverlap.shape[0] == 0:
                 padsize = self.block_size - overlap.shape[0]
@@ -72,9 +69,9 @@ class ConvolutionalReverb(Processor):
                 padsize = self.overlap.shape[0] - eoverlap.shape[0]
                 eoverlap = np.pad(eoverlap, ((0,padsize),(0,0)))
             
-            wet = y[:self.block_size] + overlap         # add the previous overlap to the output
-            dry = x[:self.block_size,:]                 # grab the dry signal
-            self.overlap = y[self.block_size:,:] + eoverlap # store the overlap for the next frame
+            wet = y[:self.block_size] + overlap             # add the previous overlap to the output
+            dry = x[:self.block_size,:]                     # grab the dry signal
+            self.overlap = y[self.block_size:,:] + eoverlap # store the overlap for the next frame (with extra overlap)
 
             wet *= self.parameters.wet_mix.value        # apply gain to wet signal
             dry *= self.parameters.dry_mix.value        # apply gain to input (dry) signal
@@ -114,7 +111,7 @@ class ConvolutionalReverb(Processor):
             flen   = fstop - fstart
 
             # if there is a fade (i.e. decay < 1.0)
-            if flen > 0 and False:
+            if flen > 0 and True:
                 fade = np.arange(flen)/flen             # normalized set of indices
                 fade = np.power(0.1, (1-fade) * 5)      # fade gain values with 100 dB of atten
                 fade = np.expand_dims(fade, 1)          # add stereo dim
@@ -122,32 +119,10 @@ class ConvolutionalReverb(Processor):
                 self.h[fstart:fstop,:] *= fade          # apply fade
                 self.h = self.h[:fstop]                 # throw away faded samples
 
-            # pad the impulse to be divsibible by block size
-            #pad = self.block_size - (self.h.shape[0]%self.block_size)
-            #self.h = np.pad(self.h, ((0,pad),(0,0)))
+            self.reset_state() # set the internal buffer to zeros
 
-            # split the impulse into blocks of size block_size
-            #nfilters = self.h.shape[0]//self.block_size
-            #self.h_new = np.empty((self.block_size*2, self.h.shape[1], nfilters))
-
-            # manually construct matrix of nfilters
-            #for n in np.arange(nfilters):
-            #    start = n * self.block_size
-            #    stop  = start + self.block_size
-            #    # zero pad each chopped impulse at the end to block_size*2 
-            #    self.h_new[:,:,n] = np.pad(self.h[start:stop,:], ((0, self.block_size),(0,0)))
-
-            #self.h = self.h_new                                         # overwrite the unraveled impulse with the chopped one
-            #self.H = fft(self.h, axis=0)                                # convert to freq domain filters
-            #X_init = np.zeros((self.h.shape))                           # create buffer to store past outputs in freq domain
-
-            if self.h.shape[0] > self.block_size:
-                overlap_shape = self.h.shape[0] - 1
-            else:
-                overlap_shape = self.h.shape[0] - 1
-
-            ovrlp_init = np.zeros((overlap_shape,self.h.shape[1]))          # create buffer for the time-domain overlap signal
-            #self.X = fft(X_init, axis=0)                               # convert zero values to freq domain
-            self.overlap = ovrlp_init                                   # store zero values input buffer
-
-
+    def reset_state(self):
+        overlap_shape = self.h.shape[0] - 1                         # overlap buffer size 
+        overlap_init = np.zeros((overlap_shape,self.h.shape[1]))    # create buffer for the time-domain overlap signal
+        self.overlap = overlap_init                                 # store zero values input buffer
+        
