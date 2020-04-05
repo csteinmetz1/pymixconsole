6af5193ae1414f9f96ce968731e6195db340207a
csteinmetz1 <csteinmetz1@gmail.com>
2020-04-05 12:01:26 +0200

    attempt at speeding up convolutional reverb

--- pymixconsole/processors/convreverb.py
+++ pymixconsole/processors/convreverb.py
@@ -4,6 +4,7 @@ import os
 import pathlib
 import warnings
 import numpy as np
+import scipy.signal
 from scipy.io import wavfile
 
 from ..parameter import Parameter
@@ -51,14 +52,29 @@ class ConvolutionalReverb(Processor):
         if self.parameters.bypass.value:
             return x
         else:
-            x = np.pad(x, ((0, self.block_size),(0,0))) # zero pad the input frame
-            self.X = np.roll(self.X, 1, axis=2)         # make space for the new frame
-            self.X[:,:,0] = fft(x, axis=0)              # store the result of the fft for current frame
-            Y = np.sum(self.X * self.H, axis=2)         # multiply inputs with filters
-            y = np.real(ifft(Y, axis=0))                # convert result to the time domain (only take real part)
-            wet = y[:self.block_size] + self.overlap    # add the previous overlap to the output
+            #x = np.pad(x, ((0, self.block_size),(0,0))) # zero pad the input frame
+            #self.X = np.roll(self.X, 1, axis=2)         # make space for the new frame
+            #self.X[:,:,0] = fft(x, axis=0)              # store the result of the fft for current frame
+            #Y = np.sum(self.X * self.H, axis=2)         # multiply inputs with filters
+            #y = np.real(ifft(Y, axis=0))                # convert result to the time domain (only take real part)
+
+            y = scipy.signal.oaconvolve(x, self.h, axes=0, mode='full')
+            
+            overlap = self.overlap[:self.block_size]
+
+            # there will be some overlap left over we need to save
+            eoverlap = self.overlap[self.block_size:]
+            if eoverlap.shape[0] == 0:
+                padsize = self.block_size - overlap.shape[0]
+                overlap = np.pad(overlap, ((0,padsize),(0,0)))
+                eoverlap = np.zeros((self.overlap.shape))
+            else:
+                padsize = self.overlap.shape[0] - eoverlap.shape[0]
+                eoverlap = np.pad(eoverlap, ((0,padsize),(0,0)))
+            
+            wet = y[:self.block_size] + overlap         # add the previous overlap to the output
             dry = x[:self.block_size,:]                 # grab the dry signal
-            self.overlap = y[self.block_size:,:]        # store the overlap for the next frame
+            self.overlap = y[self.block_size:,:] + eoverlap # store the overlap for the next frame
 
             wet *= self.parameters.wet_mix.value        # apply gain to wet signal
             dry *= self.parameters.dry_mix.value        # apply gain to input (dry) signal
@@ -107,25 +123,31 @@ class ConvolutionalReverb(Processor):
                 self.h = self.h[:fstop]                 # throw away faded samples
 
             # pad the impulse to be divsibible by block size
-            pad = self.block_size - (self.h.shape[0]%self.block_size)
-            self.h = np.pad(self.h, ((0,pad),(0,0)))
+            #pad = self.block_size - (self.h.shape[0]%self.block_size)
+            #self.h = np.pad(self.h, ((0,pad),(0,0)))
 
             # split the impulse into blocks of size block_size
-            nfilters = self.h.shape[0]//self.block_size
-            self.h_new = np.empty((self.block_size*2, self.h.shape[1], nfilters))
+            #nfilters = self.h.shape[0]//self.block_size
+            #self.h_new = np.empty((self.block_size*2, self.h.shape[1], nfilters))
 
             # manually construct matrix of nfilters
-            for n in np.arange(nfilters):
-                start = n * self.block_size
-                stop  = start + self.block_size
-                # zero pad each chopped impulse at the end to block_size*2 
-                self.h_new[:,:,n] = np.pad(self.h[start:stop,:], ((0, self.block_size),(0,0)))
+            #for n in np.arange(nfilters):
+            #    start = n * self.block_size
+            #    stop  = start + self.block_size
+            #    # zero pad each chopped impulse at the end to block_size*2 
+            #    self.h_new[:,:,n] = np.pad(self.h[start:stop,:], ((0, self.block_size),(0,0)))
 
-            self.h = self.h_new                                         # overwrite the unraveled impulse with the chopped one
-            self.H = fft(self.h, axis=0)                                # convert to freq domain filters
-            X_init = np.zeros((self.h.shape))                           # create buffer to store past outputs in freq domai
-            ovrlp_init = np.zeros((self.block_size, self.h.shape[1]))   # create buffer for the time-domain overlap signal
-            self.X = fft(X_init, axis=0)                                # convert zero values to freq domain
+            #self.h = self.h_new                                         # overwrite the unraveled impulse with the chopped one
+            #self.H = fft(self.h, axis=0)                                # convert to freq domain filters
+            #X_init = np.zeros((self.h.shape))                           # create buffer to store past outputs in freq domain
+
+            if self.h.shape[0] > self.block_size:
+                overlap_shape = self.h.shape[0] - 1
+            else:
+                overlap_shape = self.h.shape[0] - 1
+
+            ovrlp_init = np.zeros((overlap_shape,self.h.shape[1]))          # create buffer for the time-domain overlap signal
+            #self.X = fft(X_init, axis=0)                               # convert zero values to freq domain
             self.overlap = ovrlp_init                                   # store zero values input buffer
 
 
